7122c23b04d4b02016fc77068788816e8d1f4813
Jason Mehring <nrgaway@gmail.com>
2015-12-22 15:53:09 -0500

    modules: topd - Reformat

--- _modules/topd.py
+++ _modules/topd.py
@@ -20,10 +20,7 @@ import salt.template
 import salt.version
 
 from salt.exceptions import SaltRenderError
-from salt.utils.odict import (
-    OrderedDict,
-    DefaultOrderedDict
-    )
+from salt.utils.odict import (OrderedDict, DefaultOrderedDict)
 
 # Import custom libs
 from toputils import TopUtils
@@ -162,13 +159,13 @@ def render(path, opts=None, saltenv='base', sls=''):
         renderers = get_renderers(opts)
 
         salt_data = salt.template.compile_template(
-                template,
-                renderers,
-                opts['renderer'],
-                saltenv=saltenv,
-                sls=sls,
-                _pillar_rend=is_pillar(opts)
-            )
+            template,
+            renderers,
+            opts['renderer'],
+            saltenv=saltenv,
+            sls=sls,
+            _pillar_rend=is_pillar(opts)
+        )
         return salt_data
     return OrderedDict()
 
@@ -189,34 +186,41 @@ def render_top(opts, toputils):
         if not opts['default_top']:
             raise SaltRenderError(
                 'Top file merge strategy set to same, but no default_top '
-                'configuration option was set')
+                'configuration option was set'
+            )
         opts[environment] = opts['default_top']
 
     if opts.get(environment, None):
-        salt_data = render(opts['state_top'],
-                           opts=opts,
-                           saltenv=opts[environment])
+        salt_data = render(
+            opts['state_top'],
+            opts=opts,
+            saltenv=opts[environment]
+        )
         if salt_data:
             tops[opts[environment]] = salt_data
     elif opts['top_file_merging_strategy'] == 'merge':
         if opts.get('state_top_saltenv', False):
             saltenv = opts['state_top_saltenv']
-            salt_data = render(opts['state_top'],
-                               opts=opts,
-                               saltenv=saltenv)
+            salt_data = render(opts['state_top'], opts=opts, saltenv=saltenv)
             if salt_data:
                 tops[saltenv].append(salt_data)
             else:
                 log.debug('No contents loaded for env: {0}'.format(saltenv))
         else:
             for saltenv in get_envs(opts):
-                salt_data = render(opts['state_top'],
-                                   opts=opts,
-                                   saltenv=saltenv)
+                salt_data = render(
+                    opts['state_top'],
+                    opts=opts,
+                    saltenv=saltenv
+                )
                 if salt_data:
                     tops[saltenv].append(salt_data)
                 else:
-                    log.debug('No contents loaded for env: {0}'.format(saltenv))
+                    log.debug(
+                        'No contents loaded for env: {0}'.format(
+                            saltenv
+                        )
+                    )
 
     # Search initial top files for includes
     for saltenv, ctops in six.iteritems(tops):
@@ -245,7 +249,8 @@ def render_top(opts, toputils):
                     else:
                         log.debug(
                             'No contents loaded for include {0} env: {1}'
-                            .format(path, saltenv))
+                            .format(path, saltenv)
+                        )
                     done[saltenv].append(sls)
         for saltenv in pops:
             if saltenv in include:
@@ -282,7 +287,9 @@ def merge_tops(tops):
                             matches = []
                             states = set()
                             for comp in top[saltenv][tgt] + ctop[saltenv][tgt]:
-                                if isinstance(comp, dict) and comp not in matches:
+                                if isinstance(
+                                    comp, dict
+                                ) and comp not in matches:
                                     matches.append(comp)
                                 if isinstance(comp, six.string_types):
                                     states.add(comp)
@@ -290,7 +297,8 @@ def merge_tops(tops):
                             top[saltenv][tgt].extend(list(states))
                     except TypeError:
                         raise SaltRenderError(
-                            'Unable to render top file. No targets found.')
+                            'Unable to render top file. No targets found.'
+                        )
     return top
 
 
